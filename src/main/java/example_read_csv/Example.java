d547d29ac72a79730b3d8fb04e00a323a3e10fe5
carlossevero01 <carlos212427@gmail.com>
2021-08-19 21:20:13 -0300

    1

--- src/main/java/example_read_csv/Example.java
+++ src/main/java/example_read_csv/Example.java
@@ -3,9 +3,12 @@ package example_read_csv;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import ifsul.edu.br.modelo.produto;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,11 +17,12 @@ public class Example {
 
     public static void main(String[] args) throws IOException {
 
-        String fileName = "C:\\data\\Products.csv";
-
+        String fileName = "C:\\Users\\carlos\\Documents\\Aula\\3ºSemestre\\EstruturaDeDados\\Products.csv";
+        List<produto> produtos = new ArrayList<>();
         try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
             List<String[]> r = reader.readAll();
             r.forEach(x -> System.out.println(Arrays.toString(x)));
+
         } catch (CsvException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/ifsul/edu/br/modelo/produto.java
@@ -0,0 +1,35 @@
+package ifsul.edu.br.modelo;
+
+public class produto {
+    private String id;
+    private String name;
+
+    @Override
+    public String toString() {
+        return "produto{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public produto(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
